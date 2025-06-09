e8a662acf6af688836a080d3122e3518b988be8f
andres <andresnowak52@gmail.com>
2025-06-09 12:09:54 +0200

    One last experiment

--- src/custom_datasets.py
+++ src/custom_datasets.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+import random
 
 LETTER_INDICES = [
     "A",
@@ -41,6 +42,8 @@ MARKDOWN_SEPARATORS = [
     "",
 ]
 
+random.seed(42)
+
 class MCQADatasetClassification(Dataset):
     def __init__(self, data):
         self.data = data
@@ -64,6 +67,40 @@ class MCQADatasetClassification(Dataset):
             )
             + "Answer:"
         )
+
+        return {
+            "prompt": prompt,
+            "options": [
+                f" {letter}" for letter in LETTER_INDICES[: len(ex["choices"])]
+            ],
+            "correct_idx": correct_index,
+            "dataset": ex["dataset"],
+        }
+
+
+class MCQADatasetClassification_2(Dataset):
+    def __init__(self, data, template_files, jinja_env):
+        self.data = data
+        self.jinja_env = jinja_env
+        self.template_files = template_files
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        ex = self.data[idx]
+        answer_letter = ex["answer"].strip().upper()
+        correct_index = ord(answer_letter) - ord("A")
+
+
+        chosen_template = random.choice(self.template_files)
+        template = self.jinja_env.get_template(chosen_template)
+
+        choices_list = [
+            f"{key}. {choice}\n" for key, choice in zip(LETTER_INDICES, ex["choices"])
+        ]
+
+        prompt = template.render(question=ex['question'], choices_list=choices_list)
 
         return {
             "prompt": prompt,

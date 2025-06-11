bd69f039d64af1240099373b9b85523c60baefe9
Andres Nowak <andresnowak52@gmail.com>
2025-06-11 16:54:51 +0200

    added the push to hub to rest of files

--- finetune_mcqa_likelihood_rl.py
+++ finetune_mcqa_likelihood_rl.py
@@ -303,7 +303,7 @@ def train(cfg: DictConfig):
         fp16=not torch.cuda.is_bf16_supported(),
         lr_scheduler_type=cfg.training.lr_scheduler_type,
         seed=cfg.environment.seed,
-        push_to_hub=True,
+        push_to_hub=cfg.training.push_to_hub,
         hub_model_id=cfg.model.hub_model_id,
         max_completion_length=cfg.training.completion_length,
         num_generations=cfg.training.num_generations,
--- finetune_mcqa_rl_2.py
+++ finetune_mcqa_rl_2.py
@@ -334,7 +334,7 @@ def train(cfg: DictConfig):
         fp16=not torch.cuda.is_bf16_supported(),
         lr_scheduler_type="cosine",
         seed=cfg.environment.seed,
-        push_to_hub=True,
+        push_to_hub=cfg.training.push_to_hub,
         hub_model_id=cfg.model.hub_model_id,
         max_completion_length=cfg.training.completion_length,
         num_generations=cfg.training.num_generations,
